fc78ba7ec15eb2d5ca97db57df0872b979103898
SeitaHigashi <higashi110902@gmail.com>
2018-07-08 16:40:42 +0900

    ModulesAndFunctions-6.exs

---
+++ practice_question/ModulesAndFunctions-6.exs
@@ -0,0 +1,29 @@
+defmodule Chop do
+  def guess(actual, range) do
+    min..max = range
+    now = div(min+max, 2)
+    IO.puts("Is it #{now}")
+    helper(actual, range, now)
+  end
+
+  def helper(actual, _, now) when actual == now do
+    IO.puts(now)
+  end
+
+  def helper(actual, range, now) when actual < now do
+    min.._ = range
+    range = min..(now-1)
+    now = div(min+now-1, 2)
+    IO.puts("Is it #{now}")
+    helper(actual, range, now)
+  end
+
+  def helper(actual, range, now) when actual > now do
+    _..max = range
+    range = (now+1)..max
+    now = div(max+now+1, 2)
+    IO.puts("Is it #{now}")
+    helper(actual, range, now)
+  end
+
+end
