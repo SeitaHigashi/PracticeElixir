6a5a597914ee93c0a25cf586320aaac9fc499256
SeitaHigashi <higashi110902@gmail.com>
2018-07-08 18:56:26 +0900

    [finish] Chapter 6

---
+++ practice_question/ModulesAndFunctions-7.exs
@@ -0,0 +1,6 @@
+:erlang.float_to_list(3.14) |> IO.puts
+System.get_env()  |> IO.inspect
+Path.extname("ModulesAndFunctions-7.exs")  |> IO.puts
+System.cwd |> IO.puts
+# ex_json_schema
+System.cmd("ls", []) |> IO.inspect 
